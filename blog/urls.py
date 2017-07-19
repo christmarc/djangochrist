96856a4d52a77e8bd16cb8eccb6e45f39929fc31
christmarc <marco8293070282@gmail.com>
2017-07-19 19:19:44 +0200

    try again

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
