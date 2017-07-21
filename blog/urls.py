6ba3f1e025891839d28830ca6fe0efe0039ebcbb
christmarc <marco8293070282@gmail.com>
2017-07-21 03:21:10 +0200

    Add views to create/edit post inside site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
